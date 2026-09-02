d5fe14ee053747e09bb09299da7f55da5858eae3
Zubair <zubairanjum893@gmail.com>
2026-09-02 14:09:19 +0500

    add streamlit ui

---
+++ chatbot/streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from langgraph_core import chatbot
+from langchain_core.messages import HumanMessage
+
+# st.session_state -> dict -> 
+CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+# loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])       
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+    
+    ai_message = response['messages'][-1].content
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    with st.chat_message('assistant'):
+        st.text(ai_message)
